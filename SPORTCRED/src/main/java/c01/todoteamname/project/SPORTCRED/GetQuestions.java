461d1eb18bd24189ceb592c5e301dd82f4f54405
Ryan <Blasetti.ryan@gmail.com>
2020-10-13 10:54:27 -0400

    TOD-56 Added class to represent context that sends questions back to client (#9)
    Co-authored-by: Ryan Blasetti <ryan_blaset@pop-os.localdomain>

---
+++ SPORTCRED/src/main/java/c01/todoteamname/project/SPORTCRED/GetQuestions.java
@@ -0,0 +1,55 @@
+package c01.todoteamname.project.SPORTCRED;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Map;
+import org.json.JSONException;
+import org.json.JSONObject;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public class GetQuestions implements HttpHandler {
+
+  Neo4JDB nb = Neo4JDB.createInstanceOfDatabase();
+
+  public GetQuestions() {}
+
+  @Override
+  public void handle(HttpExchange r) {
+    try {
+      if (r.getRequestMethod().equals("GET")) {
+        handleGet(r);
+      }
+    } catch (Exception e) {
+      try {
+        r.sendResponseHeaders(400, -1);
+      } catch (IOException e1) {
+      }
+    }
+  }
+
+  public void handleGet(HttpExchange r) throws IOException, JSONException {
+    String body = Utils.convert(r.getRequestBody());
+    JSONObject deserialized = new JSONObject(body);
+    String numQuestions;
+
+    if (deserialized.has("numQuestions")) {
+      numQuestions = deserialized.getString("numQuestions");
+
+      Map<String, Object> resultMap = this.nb.getTriviaQuestions(r, numQuestions);
+      JSONObject json = new JSONObject(resultMap);
+      String response = json.toString();
+      r.getResponseHeaders().set("Content-Type", "application/json");
+      r.sendResponseHeaders(200, response.length());
+      OutputStream os = r.getResponseBody();
+      os.write(response.getBytes());
+      os.close();
+
+    }
+
+    else {
+      r.sendResponseHeaders(400, -1);
+    }
+
+  }
+}

c8dec4784121ea73b14e3a003f4fbc43ebc45a39
radwin01 <57123019+radwin01@users.noreply.github.com>
2020-10-16 14:22:16 -0400

    Radwin01/TOD-20 (#11)
    * TOD-20 Implemented basic web app interface to match user experience templates
    
    * TOD-20 Made slight augmentations to basic interface design, fixed minor alignment issues.
    
    Co-authored-by: Yara Radwan <yararadwan@Yaras-MacBook-Air.local>

---
+++ SPORTCRED/frontend/build/precache-manifest.cc8e6dd3fb69debf1d845dcbfd36f257.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c60bb830d7a46843aa48a15e6ef0170",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcef9119ae2e4863fc8c",
+    "url": "/static/css/main.787b5906.chunk.css"
+  },
+  {
+    "revision": "c7883d207a2573035f08",
+    "url": "/static/js/2.c768fd4a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c768fd4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcef9119ae2e4863fc8c",
+    "url": "/static/js/main.504896d3.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "1b44e2647348275165eb6cb8b22bbf07",
+    "url": "/static/media/lebron.1b44e264.jpg"
+  },
+  {
+    "revision": "00901483f9b4199c7cdb0b79f2e3982b",
+    "url": "/static/media/logo.00901483.svg"
+  }
+]);
---
+++ SPORTCRED/frontend/src/App.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import './App.css';
+import logo from './logo.svg';
+import lebron from './lebron.jpg';
+import { NavLink, Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} className="logo" alt="logo" />
+      <header>
+      <div class="container">
+        <nav>
+          <ul>
+            <li><NavLink exact activeClassName="current" to='/'>Z</NavLink></li>
+            <li><NavLink exact activeClassName="current" to='/live'>LIVE</NavLink></li>
+          </ul>
+        </nav>
+      </div>
+      </header>
+        <div class="verbar">
+          <header>
+            <div class="profile">
+              <h2>Welcome, <b>[insert name]</b></h2>
+              <p><img src={lebron} className="lebron" alt="lebron"/> ACS: XXX</p>
+              <p><b>[insert tier]</b></p>
+              <hr class="new1"></hr>
+            </div>
+            <div class="sidebar">
+              <ul>
+                <li><NavLink exact activeClassName="current" to='/my-account'>MY ACCOUNT</NavLink></li>
+                <li><NavLink exact activeClassName="current" to='/open-court'>OPEN COURT</NavLink></li>
+                <li><NavLink exact activeClassName="current" to='/trivia'>TRIVIA</NavLink></li>
+                <li><NavLink exact activeClassName="current" to='/picks'>PICKS</NavLink></li>
+                <li><NavLink exact activeClassName="current" to='/analyze'>ANALYZE</NavLink></li>
+              </ul>
+            </div>
+          </header>
+      </div>
+    <Main />
+    </div>
+  );
+}
+
+const Home = () => (
+  <div className='home'>
+  </div>
+);
+
+const MyAccount = () => (
+  <div className='my-account'>
+  </div>
+);
+
+const Trivia = () => (
+  <div className='trivia'>
+  </div>
+);
+
+const Picks = () => (
+  <div className='picks'>
+  </div>
+);
+
+const OpenCourt = () => (
+  <div className='open-court'>
+  </div>
+);
+
+const Analyze = () => (
+  <div className='analyze'>
+  </div>
+);
+
+const Live = () => (
+  <div className='live'>
+    <p>coming soon...</p>
+  </div>
+);
+
+const Main = () => (
+  <Switch>
+    <Route exact path='/' component={Home}></Route>
+    <Route exact path='/my-account' component={MyAccount}></Route>
+    <Route exact path='/open-court' component={OpenCourt}></Route>
+    <Route exact path='/trivia' component={Trivia}></Route>
+    <Route exact path='/picks' component={Picks}></Route>
+    <Route exact path='/analyze' component={Analyze}></Route>
+    <Route exact path='/live' component={Live}></Route>
+  </Switch>
+);
+
+export default App;

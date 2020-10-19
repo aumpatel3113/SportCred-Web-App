753d4230a5827c953579e29511b54e76751758ff
Ryan <Blasetti.ryan@gmail.com>
2020-10-19 15:48:07 -0400

    Blasetti-ryan/TOD-55 Implemented algorithm to generate ACS for trivia game. (#17)
    * TOD-55 Added method in UserNode class to calculate changes in ACS in accordance with Sportcred algorithm
    
    * TOD-55 Added check to catch error when setCategoryValue returns null
    
    * TOD-55 Fixed bugs in updateScore and added method to check if score is maxed in given category
    
    Co-authored-by: Ryan Blasetti <ryan_blaset@pop-os.localdomain>

--- SPORTCRED/src/main/java/c01/todoteamname/project/SPORTCRED/UserNode.java
+++ SPORTCRED/src/main/java/c01/todoteamname/project/SPORTCRED/UserNode.java
@@ -1,136 +1,221 @@
 package c01.todoteamname.project.SPORTCRED;
 
+import java.util.ArrayList;
+
 public class UserNode {
 
-	private String username, email, password, biography, picture;
-	private String Q1, Q2, Q3, Q4, Q5;
-	private double ACS, triviaScore, debateScore, pickScore, historyScore;
-	private Neo4JDB neoDB = Neo4JDB.createInstanceOfDatabase();
+  private final int MAXACS = 1100;
+  private final int MAXTRIVIA = (int) (0.1 * MAXACS);
+  private final int MAXPARTICIPATION = (int) (0.1 * MAXACS);
+  private final int MAXPICKS = (int) (0.5 * MAXACS);
+  private final int MAXDEBATE = (int) (0.3 * MAXACS);
+  private String username, email, password, biography, picture;
+  private String Q1, Q2, Q3, Q4, Q5;
+  private double ACS, triviaScore, debateScore, pickScore, historyScore;
+  private Neo4JDB neoDB = Neo4JDB.createInstanceOfDatabase();
 
-	public UserNode(String username) {
-		
-		neoDB.fillUser(this, username);
-		
-	}
+  public UserNode(String username) {
 
-	public String getUsername() {
-		return username;
-	}
+    neoDB.fillUser(this, username);
+    // this.username = username;
+    // ACS = 1000;
+    // triviaScore = 109;
 
-	public String getQ1() {
-		return Q1;
-	}
+  }
 
-	public void setQ1(String q1) {
-		Q1 = q1;
-	}
+  public String getUsername() {
+    return username;
+  }
 
-	public String getQ2() {
-		return Q2;
-	}
+  public String getQ1() {
+    return Q1;
+  }
 
-	public void setQ2(String q2) {
-		Q2 = q2;
-	}
+  public void setQ1(String q1) {
+    Q1 = q1;
+  }
 
-	public String getQ3() {
-		return Q3;
-	}
+  public String getQ2() {
+    return Q2;
+  }
 
-	public void setQ3(String q3) {
-		Q3 = q3;
-	}
+  public void setQ2(String q2) {
+    Q2 = q2;
+  }
 
-	public String getQ4() {
-		return Q4;
-	}
+  public String getQ3() {
+    return Q3;
+  }
 
-	public void setQ4(String q4) {
-		Q4 = q4;
-	}
+  public void setQ3(String q3) {
+    Q3 = q3;
+  }
 
-	public String getQ5() {
-		return Q5;
-	}
+  public String getQ4() {
+    return Q4;
+  }
 
-	public void setQ5(String q5) {
-		Q5 = q5;
-	}
+  public void setQ4(String q4) {
+    Q4 = q4;
+  }
 
-	public String getEmail() {
-		return email;
-	}
+  public String getQ5() {
+    return Q5;
+  }
 
-	public String getPassword() {
-		return password;
-	}
+  public void setQ5(String q5) {
+    Q5 = q5;
+  }
 
-	public String getBiography() {
-		return biography;
-	}
+  public String getEmail() {
+    return email;
+  }
 
-	public String getPicture() {
-		return picture;
-	}
+  public String getPassword() {
+    return password;
+  }
 
-	public double getACS() {
-		return ACS;
-	}
+  public String getBiography() {
+    return biography;
+  }
 
-	public double getTriviaScore() {
-		return triviaScore;
-	}
+  public String getPicture() {
+    return picture;
+  }
 
-	public double getDebateScore() {
-		return debateScore;
-	}
+  public double getACS() {
+    return ACS;
+  }
 
-	public double getPickScore() {
-		return pickScore;
-	}
+  public double getTriviaScore() {
+    return triviaScore;
+  }
 
-	public double getHistoryScore() {
-		return historyScore;
-	}
+  public double getDebateScore() {
+    return debateScore;
+  }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+  public double getPickScore() {
+    return pickScore;
+  }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+  public double getHistoryScore() {
+    return historyScore;
+  }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+  public void setUsername(String username) {
+    this.username = username;
+  }
 
-	public void setBiography(String biography) {
-		this.biography = biography;
-	}
+  public void setEmail(String email) {
+    this.email = email;
+  }
 
-	public void setPicture(String picture) {
-		this.picture = picture;
-	}
+  public void setPassword(String password) {
+    this.password = password;
+  }
 
-	public void setACS(double aCS) {
-		ACS = aCS;
-	}
+  public void setBiography(String biography) {
+    this.biography = biography;
+  }
 
-	public void setTriviaScore(double triviaScore) {
-		this.triviaScore = triviaScore;
-	}
+  public void setPicture(String picture) {
+    this.picture = picture;
+  }
 
-	public void setDebateScore(double debateScore) {
-		this.debateScore = debateScore;
-	}
+  public void setACS(double aCS) {
+    ACS = aCS;
+  }
 
-	public void setPickScore(double pickScore) {
-		this.pickScore = pickScore;
-	}
+  public void setTriviaScore(double triviaScore) {
+    this.triviaScore = triviaScore;
+  }
 
-	public void setHistoryScore(double historyScore) {
-		this.historyScore = historyScore;
-	}
-	
+  public void setDebateScore(double debateScore) {
+    this.debateScore = debateScore;
+  }
+
+  public void setPickScore(double pickScore) {
+    this.pickScore = pickScore;
+  }
+
+  public void setHistoryScore(double historyScore) {
+    this.historyScore = historyScore;
+  }
+
+  private ArrayList<Integer> setCategoryValue(String category) {
+    ArrayList<Integer> retArray = new ArrayList<>();
+    switch (category) {
+      case "trivia":
+        retArray.add(MAXTRIVIA);
+        retArray.add((int) triviaScore);
+        return retArray;
+      case "debate":
+        retArray.add(MAXDEBATE);
+        retArray.add((int) debateScore);
+        return retArray;
+      case "participation":
+        retArray.add(MAXPARTICIPATION);
+        retArray.add((int) historyScore);
+        return retArray;
+      case "picks":
+        retArray.add(MAXPICKS);
+        retArray.add((int) pickScore);
+        return retArray;
+    }
+    return null;
+  }
+
+  public int updateScore(int scoreChange, String category) {
+    ArrayList<Integer> valueArray = setCategoryValue(category);
+    if (valueArray == null) {
+      return 500;
+    }
+    int curr = valueArray.get(1);
+    int max = valueArray.get(0);
+    int newScore = curr + scoreChange;
+    int update = scoreChange;
+
+    if (newScore >= max) {
+      update = max - curr;
+    } else if (newScore <= 0) {
+      update = 0 - curr;
+    }
+
+    int retValue = neoDB.updateScore(username, Integer.toString((int) ACS + update),
+        Integer.toString((int) curr + update), category);
+    if (retValue == 200) {
+      ACS += update;
+      switch (category) {
+        case "trivia":
+          triviaScore += update;
+          break;
+        case "debate":
+          debateScore += update;
+          break;
+        case "participation":
+          historyScore += update;
+          break;
+        case "picks":
+          pickScore += update;
+          break;
+      }
+    }
+    return retValue;
+  }
+
+  public boolean isMaxed(String category) {
+    switch (category) {
+      case "trivia":
+        return triviaScore == MAXTRIVIA;
+      case "debate":
+        return debateScore == MAXDEBATE;
+      case "participation":
+        return historyScore == MAXPARTICIPATION;
+      case "picks":
+        return pickScore == MAXPICKS;
+    }
+    return false;
+  }
+
 }
